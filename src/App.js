df948e38400aa4dde2ab5138c168a349169c884d
rohit-kashyap-1 <r23.tgc@gmail.com>
2024-08-02 15:56:03 +0530

    mapping

--- src/App.js
+++ src/App.js
@@ -5,32 +5,27 @@ import { useState } from 'react';
 //state are the variables to store values in react component
 function App() {
 
-  //e.preventDefault() 
-  let [digit,setDigit] = useState(0)
-  let [factorial,setFactorial] = useState(1)
+  let [users, setUsers] = useState([
+    "Rohit kumar",
+    "Pawan kumar",
+    "Haider",
+    "Devesh kumar",
+    "Shweta kumari",
+    "Dolly sharma",
+    "piyush sharma"
+  ])
+  //mapping of a list of users
 
-  const handleInput  =(event)=>{
-    setDigit(event.target.value)
-  }
-
-  const getFactorial = ()=>{
-    //5 = 5x4x3x2x1
-    let fact = 1
-     if(digit!=0){
-       for(let i=digit;i>=1;i--){
-        fact = fact*i;
-       }
-
-       setFactorial(fact)
-     }
-  }
-  
   return (
     <div className="App p-4">
-      <h1 className='display-1'>{factorial}</h1>
-      <input type='text'  value={digit} onChange={handleInput} />
-      <button onClick={getFactorial}>Get FActorial</button>
-
+      <ul>
+        {users.map(function (item) {
+          return <li>
+                    <div>{item}</div>
+                    <div></div>
+                  </li>
+        })}
+      </ul>
     </div>
   );
 }
