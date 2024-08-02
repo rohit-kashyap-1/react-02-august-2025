d124b6a8f704d79732d5b2ad719ccf83ca9c3839
rohit-kashyap-1 <r23.tgc@gmail.com>
2024-08-02 16:03:26 +0530

    render list from api

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 //state are the variables to store values in react component
 function App() {
 
-  let [users, setUsers] = useState([
-    "Rohit kumar",
-    "Pawan kumar",
-    "Haider",
-    "Devesh kumar",
-    "Shweta kumari",
-    "Dolly sharma",
-    "piyush sharma"
-  ])
-  //mapping of a list of users
+  const [users,setUsers] = useState([])
+
+  useEffect(function(){
+
+    fetch('https://dummyjson.com/users?select=firstName').then((response)=>response.json()).then((data)=>{
+      setUsers(data.users)
+    })
+
+  },[])
+
+  let count = 1
 
   return (
     <div className="App p-4">
+     
       <ul>
-        {users.map(function (item) {
-          return <li>
-                    <div>{item}</div>
+        {
+        users.map(function (item) {
+          return <li key={count++}>
+                    <div>{item.firstName}</div>
                     <div></div>
                   </li>
         })}
